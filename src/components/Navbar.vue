c4ee2c455dc80026177142313b720372381d372a
Philip Johnson <johnson.philip.louis@gmail.com>
2020-03-26 15:09:23 -0500

    basic scaffolding, fonts, nav

---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+    <b-navbar wrapper-class="container">
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+              <Logo class="nav__logo" />
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+  
+        </template>
+
+        <template slot="end">
+            <b-navbar-item href="#">
+              About
+            </b-navbar-item>
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary">
+                        Download App
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+
+<script>
+import Logo from '~/assets/images/logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style lang="scss" scoped>
+.nav {
+  &__logo {
+    height: 50px;
+  }
+}
+</style>
---
+++ src/layouts/Default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="layout">
+    <!-- This is the navigation bar component -->
+    <Navbar />
+    <!-- the section adds vertical padding to your content -->
+    <section class="section">
+      <!-- the container class adds horizontal padding when not on mobile -->
+      <div class="container">
+        <!-- the content class formats normal HTML elements underneath it to match Bulma's style -->
+        <div class="content">
+          <!-- Page content is added here -->
+          <slot />
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Navbar from "~/components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<style lang="scss"></style>
