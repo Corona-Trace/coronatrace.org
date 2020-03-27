7bfd9392086207cf3d94a0777b3164ed1191e838
Philip Johnson <johnson.philip.louis@gmail.com>
2020-03-27 15:50:10 -0500

    disable lazy load of above fold image

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -24,6 +24,7 @@
           <TraceBubbles class="bubbles" />
           <g-image
             class="push-notification"
+            :immediate="true"
             src="~/assets/images/PushNotification.png"
           ></g-image>
         </div>
