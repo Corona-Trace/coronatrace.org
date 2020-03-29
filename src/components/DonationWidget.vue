70071939e70a1daf23c13130b07a8f3f91eef380
Philip Johnson <johnson.philip.louis@gmail.com>
2020-03-29 16:22:12 -0500

    some dry changes

---
+++ src/components/DonationWidget.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="section donate__widget">
+    <div class="container">
+      <div class="columns">
+        <div class="column"></div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ src/pages/Donate.vue
@@ -0,0 +1,28 @@
+<template>
+  <Layout>
+    <div class="donate">
+      <PageHeader title="Donate Now">
+        Your donation will help save lives. Coronatrace is completely a
+        volunteer effort made possible by dozen’s of folks who want to make a
+        difference. Every penny will go toward operating expenses like server
+        costs, development, and distribution.
+      </PageHeader>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import PageHeader from '~/components/PageHeader.vue'
+
+export default {
+  components: {
+    PageHeader
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
