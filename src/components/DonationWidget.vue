1922305b2c9163219c34a8372f3d878a8480e46c
Philip Johnson <johnson.philip.louis@gmail.com>
2020-03-30 11:49:50 -0500

    final commit

--- src/components/DonationWidget.vue
+++ src/components/DonationWidget.vue
@@ -1,16 +1,177 @@
 <template>
   <section class="section donate__widget">
-    <div class="container">
+    <div class="container container__large-margins">
       <div class="columns">
-        <div class="column"></div>
+        <div class="column">
+          <div class="donate__widget-heading">
+            <h2 class="title is-4">How much would you like to give?</h2>
+            <p class="subtitle">
+              The more you give, the faster we can’t launch this effort to the
+              public.
+            </p>
+          </div>
+          <div class="widget">
+            <div class="widget__note">
+              Select the amount you would like to donate:
+            </div>
+            <div class="columns is-multiline widget__wrapper">
+
+                <div 
+                    v-for="amount in donationAmounts"
+                    class="column donation-selector"
+                    :class="{ selected: donationAmountSelected == amount }"
+                    data-amount="5"
+                    @click="setDonationAmount(amount)">
+                    ${{ amount }}
+                </div>
+
+                <b-button 
+                  class="donation-selector" 
+                  @click="setDonationAmount('Other Amount')" 
+                  :class="{ selected: donationAmountSelected == 'Other Amount' }">
+                  Other Amount
+                </b-button>
+
+                <b-field v-show="donationAmountSelected == 'Other Amount'" label-position="on-border" label="Other Amount" class="donation-input-container">
+                    <b-input
+                    class="donation-input"
+                    placeholder="Numeral"
+                    v-cleave="masks.numeral"
+                    v-model="customDonationAmount"
+                    custom-class="donation-currency-input">
+                    </b-input>
+                </b-field>
+
+              </div>
+            </div>
+
+            <div class="widget__cta">
+              <b-button type="is-primary" size="is-large" expanded>COMPLETE DONATION</b-button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-export default {}
+import { Money } from 'v-money'
+import Cleave from 'cleave.js'
+
+const cleave = {
+  name: 'cleave',
+  bind(el, binding) {
+    const input = el.querySelector('input')
+    input._vCleave = new Cleave(input, binding.value)
+  },
+  unbind(el) {
+    const input = el.querySelector('input')
+    input._vCleave.destroy()
+  }
+}
+
+export default {
+  directives: { cleave },
+  data: function() {
+    return {
+      donationAmounts: [5, 10, 20, 50, 100, 250, 500],
+      donationAmountSelected: 20,
+      customDonationAmountRaw: null,
+      customDonationAmount: null,
+      value: '',
+      rawValue: '',
+      masks: {
+        creditCard: { creditCard: true },
+        numeral: {
+          numeral: true,
+          numeralThousandsGroupStyle: 'thousand',
+          prefix: '$ '
+        },
+        custom: {
+          delimiters: ['.', '.', '-'],
+          blocks: [3, 3, 3, 2],
+          numericOnly: true
+        }
+      }
+    }
+  },
+  methods: {
+    setDonationAmount(amount) {
+      this.donationAmountSelected = amount
+    },
+    onInput(event) {
+      this.customDonationAmountRaw = event.target._vCleave.getRawValue()
+      this.customDonationAmount = event.target._vCleave.getFormattedValue()
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
+.section {
+  padding-top: 0;
+  padding-bottom: 4rem;
+}
+
+.widget {
+  &__wrapper {
+    margin-top: 0.5rem;
+  }
+
+  &__cta {
+    margin-top: 3rem;
+
+    button {
+      justify-content: center;
+    }
+  }
+}
+
+.donation-selector {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid $blue;
+  border-radius: 6px;
+  margin: 0.25rem;
+  color: $blue;
+  font-weight: bold;
+  cursor: pointer;
+  padding: 0.75rem;
+  font-size: 1.25rem;
+  text-align: center;
+  line-height: 1.5rem;
+  flex: 1 0 47%;
+
+  &::placeholder {
+    color: $blue;
+    line-height: 1.5rem;
+  }
+
+  &:hover {
+    background-color: lighten($blue, 50%);
+  }
+
+  &.selected {
+    background-color: $blue;
+    color: $white;
+  }
+}
+
+.donation-input-container {
+  width: 100%;
+  margin: 0.5rem 0.25rem;
+
+  .donation-input {
+    border: 2px solid $blue !important;
+    border-radius: 6px;
+
+    > * {
+      color: #313ebd;
+      font-weight: bold;
+      font-size: 1.2rem;
+    }
+  }
+}
 </style>
--- src/pages/Donate.vue
+++ src/pages/Donate.vue
@@ -1,22 +1,26 @@
 <template>
   <Layout>
     <div class="donate">
-      <PageHeader title="Donate Now">
+      <PageHeader title="Donate Now" largeMargins>
         Your donation will help save lives. Coronatrace is completely a
         volunteer effort made possible by dozen’s of folks who want to make a
         difference. Every penny will go toward operating expenses like server
         costs, development, and distribution.
       </PageHeader>
+
+      <DonationWidget />
     </div>
   </Layout>
 </template>
 
 <script>
 import PageHeader from '~/components/PageHeader.vue'
+import DonationWidget from '~/components/DonationWidget.vue'
 
 export default {
   components: {
-    PageHeader
+    PageHeader,
+    DonationWidget
   },
   data() {
     return {}
@@ -25,4 +29,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.page-header {
+  .section {
+    padding-top: 13rem;
+    padding-bottom: 3rem;
+  }
+}
 </style>
