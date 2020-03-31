e871ab2d3e6b2d9bb2ffbd75cfaeb84c45c58a20
Philip Johnson <johnson.philip.louis@gmail.com>
2020-03-31 13:28:04 -0500

    fix mobile

--- src/components/contributors/IndividualContributors.vue
+++ src/components/contributors/IndividualContributors.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="columns is-variable is-multiline is-4 is-mobile">
+  <div class="columns is-multiline is-mobile">
     <div
       v-for="edge in $static.allIndividual.edges"
       :key="edge.node.id"
@@ -8,11 +8,15 @@
       <div class="contributor">
         <div class="contributor__heading">
           <h4>{{ edge.node.fields.Name }}</h4>
-          <a :href="edge.node.fields.LinkedIn">
+          <a v-if="edge.node.fields.LinkedIn" :href="edge.node.fields.LinkedIn">
             <b-icon icon="linkedin"></b-icon>
           </a>
         </div>
-        <p class="contributor__skill">{{ edge.node.fields.Skill }}</p>
+        <div class="contributor__footer">
+          <p v-if="edge.node.fields.Skill" class="contributor__skill">
+            {{ edge.node.fields.Skill }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -23,40 +27,49 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
-.is-mobile {
-  .contributor {
+.column {
+  padding: 0.25rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 0 0 100% !important;
+
+  @include from($desktop) {
+    flex: none !important;
+    padding: 0.75rem;
+  }
+}
+
+.contributor {
+  padding: 0.5rem 1rem;
+  background-color: $blue-light;
+  border-radius: 6px;
+  width: 100%;
+
+  &__heading {
     display: flex;
-    flex-direction: column;
-    justify-content: center;
     align-items: center;
-    flex: 0 0 50% !important;
-    padding: 0.5rem 1rem;
-    background-color: $blue-light;
-    border-radius: 6px;
+    justify-content: space-between;
+    width: 100%;
 
-    @include from($desktop) {
-      flex: none !important;
-    }
-
-    &__heading {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      width: 100%;
-
-      h4 {
-        font-size: 1.125rem;
-        font-weight: 500;
-        margin: 0;
-      }
-    }
-
-    p {
+    h4 {
+      font-size: 1.125rem;
+      font-weight: 500;
       margin: 0;
-      font-size: 0.875rem;
-      color: $grey;
-      width: 100%;
     }
+  }
+
+  &__footer {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  p {
+    margin: 0;
+    font-size: 0.875rem;
+    color: $grey;
+    width: 100%;
   }
 }
 </style>
