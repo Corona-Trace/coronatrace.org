2b40af52fadf593722fcf82382071e98aaa15740
Ed Parsons <ed@acutulus.co>
2020-04-06 18:47:20 -0700

    Adding page

---
+++ src/pages/NextSteps.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <div class="about">
+      <PageHeader
+        :title="$page.allContentfulAboutPage.edges[0].node.heading"
+        largeMargins
+      >
+        {{ $page.allContentfulAboutPage.edges[0].node.subheading }}
+      </PageHeader>
+
+      <AboutContentBlock
+        :title="edge.node.heading"
+        v-for="edge in $static.allContentfulAboutContentBlock.edges"
+        :key="edge.node.id"
+        :alternateBackground="edge.node.darkBackground"
+      >
+        <div v-html="richTextToHTML(edge.node.content)"></div>
+      </AboutContentBlock>
+
+      <AboutCallToAction />
+    </div>
+  </Layout>
+</template>
+
+<script>
+</script>
+
+<page-query></page-query>
+
+<style lang="scss" scoped>
+
+</style>
