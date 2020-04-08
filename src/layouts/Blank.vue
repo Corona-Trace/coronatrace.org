715e181af8b06e0aba8b57c23a9f625b798336d2
Gideon Baldridge <gideon.bridge@gmail.com>
2020-04-07 17:29:05 -0700

    alternate header

---
+++ src/layouts/Blank.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="layout">
+
+    <transition name="fade" appear>
+      <main>
+        <slot />
+      </main>
+    </transition>
+
+  </div>
+</template>
+
+<style lang="scss">
+.fade-enter-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+</style>
