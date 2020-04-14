8071f147c93f81d0afac8bfac244aad39a2442da
Philip Johnson <johnson.philip.louis@gmail.com>
2020-04-14 08:06:24 -0500

    finally some working purgecss rules

--- purgecss.config.js
+++ purgecss.config.js
@@ -12,18 +12,19 @@ module.exports = {
     './node_modules/buefy/input/*.(vue|js)',
     './node_modules/buefy/select/*.(vue|js)',
     './node_modules/v-lazy-image/dist/*.js',
-    './node_modules/bulma-scss/components/modal/*.scss'
+    './node_modules/bulma-scss/components/_modal.scss',
+    './node_modules/bulma-scss/components/_message.scss',
+    './node_modules/bulma-scss/components/_dropdown.scss',
+    './node_modules/bulma-scss/form/*.scss'
   ],
   css: ['./dist/assets/css/*.css'],
   whitelistPatterns: [
     /-(leave|enter|appear)(|-(to|from|active))$/,
     /^(?!cursor-move).+-move$/,
     /^router-link(|-exact)-active$/,
-    /^g-link.+/,
-    /^modal.*/,
-    /^is-active.*/,
-    /^email-signup.*/
+    /^g-link.+/
   ],
+  whitelistPatternsChildren: [/^modal.*/, /^is-active.*/, /^email-signup.*/],
   rejected: true,
   keyframes: true,
   variables: true
