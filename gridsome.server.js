2a031874209b78061fe939a7aa3003e92a45f390
Philip Johnson <johnson.philip.louis@gmail.com>
2020-05-01 12:35:22 -0500

    stop generating the home page

--- gridsome.server.js
+++ gridsome.server.js
@@ -25,13 +25,15 @@ module.exports = function(api) {
     `)
 
     data.allStoryblokEntry.edges.forEach(({ node }) => {
-      createPage({
-        path: `/${node.full_slug}`,
-        component: './src/templates/StoryblokEntry.vue',
-        context: {
-          id: node.id
-        }
-      })
+      if (node.full_slug != 'home') {
+        createPage({
+          path: `/${node.full_slug}`,
+          component: './src/templates/StoryblokEntry.vue',
+          context: {
+            id: node.id
+          }
+        })
+      }
     })
   })
 }
