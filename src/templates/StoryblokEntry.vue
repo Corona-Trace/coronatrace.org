2fb68501f4b774450d155d43298db91b89933b7b
Philip Johnson <johnson.philip.louis@gmail.com>
2020-05-10 09:51:30 -0500

    more seo

--- src/templates/StoryblokEntry.vue
+++ src/templates/StoryblokEntry.vue
@@ -14,6 +14,7 @@
 import { mapGetters, mapActions, mapState } from 'vuex'
 import EmailSignupModal from '~/components/email-signup/EmailSignupModal.vue'
 import Footer from '~/components/Footer.vue'
+import { getMetadataToPage } from '../utils/meta-tags'
 
 export default {
   props: {
@@ -27,16 +28,16 @@ export default {
     Footer
   },
   metaInfo() {
-    return {
-      title: this.$page.storyblokEntry.content.MetaTitle,
-      meta: [
-        {
-          key: 'description',
-          name: 'description',
-          content: this.$page.storyblokEntry.content.MetaDescription
-        }
-      ]
+    const config = {
+      title: this.story.content.MetaTitle,
+      description: this.story.content.MetaDescription
     }
+    if (this.story.full_slug == 'home') {
+      config['path'] = '/'
+    } else {
+      config['path'] = `/${this.story.full_slug}`
+    }
+    return getMetadataToPage(config)
   },
   computed: {
     ...mapGetters(['emailModalActive']),
